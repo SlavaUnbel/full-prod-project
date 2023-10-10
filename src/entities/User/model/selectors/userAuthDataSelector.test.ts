b8eb6c6f9fa06a1384bb3c26d856d459b2b7a56b
Slava Levkovich <slavlen1999@gmail.com>
2023-10-10 10:17:33 +0300

    feat(tests): cover user entity with tests

---
+++ src/entities/User/model/selectors/userAuthDataSelector.test.ts
@@ -0,0 +1,32 @@
+import { ApplicationState } from 'app/providers/StoreProvider';
+
+import { userAuthDataSelector } from './userSelector';
+
+const authData = {
+    id: '1',
+    username: 'admin',
+};
+
+describe('userAuthDataSelector', () => {
+    it('should return data', () => {
+        const state: DeepPartial<ApplicationState> = {
+            user: {
+                authData,
+            },
+        };
+
+        const result = userAuthDataSelector(state as ApplicationState);
+
+        expect(result).toBe(authData);
+    });
+
+    it('should return undefined if user state is empty', () => {
+        const state: DeepPartial<ApplicationState> = {
+            user: {},
+        };
+
+        const result = userAuthDataSelector(state as ApplicationState);
+
+        expect(result).toBe(undefined);
+    });
+});
---
+++ src/entities/User/model/slice/userSlice.test.ts
@@ -0,0 +1,39 @@
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+
+import { UserSchema } from '../types/user';
+import { userActions, userReducer } from './userSlice';
+
+const data = {
+    id: '1',
+    username: 'admin',
+};
+
+describe('userSlice', () => {
+    it('should set auth data', () => {
+        const state: DeepPartial<UserSchema> = {};
+        const result = userReducer(state as UserSchema, userActions.setAuthData(data));
+
+        expect(result).toEqual({ authData: data });
+    });
+
+    it('should init auth data', () => {
+        const state: DeepPartial<UserSchema> = {};
+        localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(data));
+        const result = userReducer(state as UserSchema, userActions.initAuthData());
+
+        const user = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+
+        expect(result).toEqual({ authData: JSON.parse(user) });
+    });
+
+    it('should logout', () => {
+        const state: DeepPartial<UserSchema> = { authData: data };
+        const result = userReducer(state as UserSchema, userActions.logout());
+        localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+
+        const user = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+
+        expect(result).toEqual({ authData: undefined });
+        expect(user).toBe(undefined);
+    });
+});
