198c46f896a40a92ab6209c7629a6f6b75e15a45
Slava Levkovich <slavlen1999@gmail.com>
2023-11-21 17:11:25 +0300

    feat(redux): add build slice and build selector

--- src/shared/lib/store/buildSlice.ts
+++ src/shared/lib/store/buildSlice.ts
@@ -1,3 +1,26 @@
-export function buildSlice(slice) {
+import {
+    CreateSliceOptions, SliceCaseReducers, bindActionCreators, createSlice,
+} from '@reduxjs/toolkit';
+import { useMemo } from 'react';
 
+import { useAppDispatch } from '../hooks/useAppDispatch';
+
+export function buildSlice<
+    State,
+    CaseReducers extends SliceCaseReducers<State>,
+    Name extends string = string
+    >(options: CreateSliceOptions<State, CaseReducers, Name>) {
+    const slice = createSlice(options);
+
+    const useActions = (): typeof slice.actions => {
+        const dispacth = useAppDispatch();
+
+        // @ts-ignore
+        return useMemo(() => bindActionCreators(slice.actions, dispacth), [dispacth]);
+    };
+
+    return {
+        ...slice,
+        useActions,
+    };
 }
